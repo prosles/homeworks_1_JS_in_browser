f19f4e1cd975d18a8d12b52ef915686a8f8aac88
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-07-14 22:19:54 +0300

    Добавлено решение второй задачи по тебе Объект события

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    const key = document.querySelector('html');
+    const func = (e) => {
+      if(e.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    }
+    key.addEventListener('keyup', func);
   }
 
   success() {
