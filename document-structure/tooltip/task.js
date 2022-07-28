049162199df0f1794b9c258afd52603f820cd956
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-07-28 21:58:45 +0300

    Добавлено решение к первой задачи по теме Изменение структуры HTML-документа

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,15 @@
+const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+const tooltip = document.querySelector('.tooltip');
+ 
+hasTooltip.forEach(item => item.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { top, left } = item.getBoundingClientRect() 
+  //Координаты рассчитываются относительного видимой части страницы без учета прокрутки 
+  if(tooltip.textContent === e.currentTarget.title) { //определяет текущий элемент DOM, в котором в настоящий момент обрабатывается событие.
+    tooltip.classList.toggle('tooltip_active');
+  } else {
+    tooltip.textContent = e.currentTarget.title;
+    tooltip.style.top = (top + 20 + 'px');
+    tooltip.style.left = (left + 'px');
+    tooltip.classList.add('tooltip_active');
+  }
