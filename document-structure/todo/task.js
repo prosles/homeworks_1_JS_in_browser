6820d292c0a8276d393cc98696dfc083950c52ba
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-07-28 22:10:09 +0300

    Добавлено решение ко второй задачи по теме Изменение структуры HTML-документа

---
+++ document-structure/todo/task.js
@@ -0,0 +1,38 @@
+const tasksInput = document.getElementById('task__input');
+const tasksAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+const taskListAdd = (text) => {
+    tasksList.insertAdjacentHTML('beforeEnd', `<div class="task">
+        <div class="task__title">
+        ${text}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>`);
+    tasksInput.value = '';
+    const task = Array.from(document.querySelectorAll('.task'));
+    const taskRemove = Array.from(document.querySelectorAll('.task__remove'));
+    let taskId = task.length - 1;
+    taskRemove[taskId].addEventListener('click', (e) => {
+        e.preventDefault();
+        task[taskId].remove();
+    });
+}
+
+tasksAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(e.currentTarget && tasksInput.value.trim().length !== 0) {
+        taskListAdd(tasksInput.value);
+    } else if(e.currentTarget) {
+        tasksInput.value = '';
+    }
+});
+
+tasksInput.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if(e.key === 'Enter' && tasksInput.value.trim().length !== 0) {   
+        taskListAdd(tasksInput.value);
+    } else if(e.key === 'Enter') {
+        tasksInput.value = '';
+    }
+});
