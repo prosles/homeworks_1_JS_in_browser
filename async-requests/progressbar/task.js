f3e42b3d252f4c1c41628d7162bf587a0a86fd28
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-08-05 22:38:53 +0300

    Добавлена плавная загрузка. Изменена третья задача по теме Асинхронные запросы

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,12 +4,13 @@ form.addEventListener( 'submit', (e) => {
     let formData = new FormData(form);
     const progress = document.getElementById( 'progress' );
     const xhr = new XMLHttpRequest();
-      
+
     e.preventDefault();
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    xhr.upload.onprogress = function(event){
-        let percent = event.loaded/event.total;
-        progress.value = percent;
+    
+    xhr.onprogress = function(event){
+        progress.value = event.loaded/ 10000000;
         }
-        xhr.send(formData);
+        
+    xhr.send(formData);
 })
