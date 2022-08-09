3e7ba2dd778f16cbfd5f6bfa95f13907eab1f96a
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-08-09 11:56:09 +0300

    Редактирование третьей задачи, добавлена очистка

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,49 +1,53 @@
-const signin = document.getElementById('signin');
-const signinForm = document.getElementById('signin__form');
+const signin = document.getElementById('signin'); 
+const signinForm = document.getElementById('signin__form'); 
+const welcome = document.getElementById('welcome'); 
+const userId = document.getElementById('user_id'); 
+const signinButton =  document.getElementById('signin__btn'); 
 
-const welcome = document.getElementById('welcome');
-const userId = document.getElementById('user_id');
 
-const signoutButton = document.getElementById('signout__btn');
+const signoutButton = document.createElement('button');
+signoutButton.className = 'btn';
+signoutButton.textContent = 'Очистка';
+signoutButton.style.display = 'block';
+signoutButton.style.marginLeft = 'auto';
+signoutButton.style.marginRight = 'auto';
+signoutButton.style.marginTop = '2em';
 
-signinForm.addEventListener('submit', e => {
+function input(id) {
+  signin.classList.remove('signin_active');
+  welcome.classList.add('welcome_active');
+  userId.textContent = id;
+  welcome.append(signoutButton);
+}
+if (localStorage.id) {
+  input(localStorage.id);
+}
+
+signinButton.addEventListener('click', (e) => {
   e.preventDefault();
 
   const formData = new FormData(signinForm);
   const xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php'); 
-  
-  xhr.addEventListener('readystatechange', function() {     
-    if (this.readyState == 4 && this.status == 200) {
-      const answer = JSON.parse(this.responseText);
-      if (!answer.success) {
-        alert('Неверный логин/пароль');
-        return;
-      }
-    
-      localStorage.userId = answer.user_id;
-      userId.innerText = localStorage.userId;
-      signin.classList.remove('signin_active');
-      welcome.classList.add('welcome_active');          
-        
-    }   
-  });  
+  xhr.responseType = 'json';
   xhr.send(formData);
-  
-  for (const input of signinForm.querySelectorAll('input')) {
-    input.value = '';
-  }
+  xhr.onreadystatechange = () => {     
+    if (xhr.readyState ===  xhr.DONE && xhr.status == 200) {
+      if (xhr.response.success) {
+        localStorage.setItem('id', xhr.response.user_id);
+        input(localStorage.id);
+      } else {
+        alert('Неверный логин/пароль');
+      }
+      signinForm.reset();
+    }   
+  };  
 });
 
-signoutButton.addEventListener('click', () => {
-  delete localStorage.userId;
+signoutButton.addEventListener('click', e => {
+  e.preventDefault();
+  localStorage.removeItem('id');
   welcome.classList.remove('welcome_active'); 
   signin.classList.add('signin_active');  
 });
 
-if (localStorage.userId !== undefined) {  
-  welcome.classList.add('welcome_active');
-  userId.innerText = localStorage.userId;  
-} else {
-  signin.classList.add('signin_active');
-}
